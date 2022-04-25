eaef571655718e2f0a8171161ec5611c7b5f2cc9
iBookdrupal <ternguh@gmail.com>
2022-04-25 21:47:38 +0100

    Create, read and delete completed

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -41,16 +41,14 @@ const updateGoal = asyncHandler(async (req, res) => {
     throw new Error("Goal not found");
   }
 
-  const user = await User.findById(req.user.id);
-
   //check for user before update
-  if (!user) {
+  if (!req.user) {
     res.status(401);
     throw new Error("User not found ");
   }
 
   //make sure the logged in user matches the goal user
-  if (goal.user.toString() !== user.id) {
+  if (goal.user.toString() !== req.user.id) {
     res.status(401);
     throw new Error("User not authorized");
   }
@@ -74,16 +72,14 @@ const deleteGoal = async (req, res) => {
     throw new Error("Goal not found");
   }
 
-  const user = await User.findById(req.user.id);
-
   //check for user before update
-  if (!user) {
+  if (!req.user) {
     res.status(401);
     throw new Error("User not found ");
   }
 
   //make sure the logged in user matches the goal user
-  if (goal.user.toString() !== user.id) {
+  if (goal.user.toString() !== req.user.id) {
     res.status(401);
     console.log(goal.user);
     throw new Error("User not authorized");
